93c887b05b28016f9bb6955c01e610610c4fee26
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2023-11-03 00:45:57 +0530

    minor changes at home 3 nov-2023

--- app/components/navbar/sidebarDashboard.js
+++ app/components/navbar/sidebarDashboard.js
@@ -11,7 +11,7 @@ const Sidebar1 = ({isMobileSidebarOpen}) => {
   const pathname = usePathname()
   return (
     <>
-      <div style={{  height: "100vh",backgroundColor:'gray' }}>
+      <div style={{  height: "100vh" }} className="bg-white">
         <div className={`hidden md:flex flex-col h-full hover:overflow-y-auto overflow-hidden `}>
             <div className="px-6 flex items-center min-w-full h-16">
               <img
@@ -26,7 +26,7 @@ const Sidebar1 = ({isMobileSidebarOpen}) => {
                 {/* <h1 className="px-6 font-medium text-black">Home</h1> */}
                 {data.slice(0,5).map((item, index) => (
                   <div key={index}>
-                    <a href={item.path} className={`flex items-center transition duration-600 hover:text-red-600 hover:scale-110 hover:text-lg ease-in-out no-underline ${pathname === item.path ? 'border-l-4 border-red-600 text-red-600' : 'text-white'}  p-3 space-x-2 text-sm  cursor-pointer   
+                    <a href={item.path} className={`flex items-center transition duration-600 hover:text-red-400 hover:scale-110 hover:text-lg ease-in-out no-underline ${pathname === item.path ? 'border-l-4 border-red-600 text-red-400 font-bold' : 'text-gray-600'}  p-3 space-x-2 text-sm  cursor-pointer   
                      `}
                     >
                      <span>{item.icon}</span> 
@@ -37,7 +37,7 @@ const Sidebar1 = ({isMobileSidebarOpen}) => {
                 <h2 className="p-3 font-bold text-blue-800 text-md">Free Resources</h2>
                       {data.slice(5,8).map((item, index) => (
                   <div key={index}>
-                    <a href={item.path}  className={`flex items-center transition duration-400 hover:text-red-600 hover:scale-110 hover:text-lg no-underline ${pathname === item.path ? 'border-l-4 border-red-600 text-red-600' : 'text-white'}   p-3 space-x-2 text-sm  cursor-pointer  
+                    <a href={item.path}  className={`flex items-center transition duration-400 hover:text-red-400 hover:scale-110 hover:text-lg no-underline ${pathname === item.path ? 'border-l-4 border-red-600 text-red-400 font-bold' : 'text-gray-600'}   p-3 space-x-2 text-sm  cursor-pointer  
                      `}
                     >
                      <span>{item.icon}</span> 
